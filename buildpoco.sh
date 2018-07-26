952a9678ca0295bd6579035cc3048b40f0d23ef6
Mark Buckaway <mark@buckaway.ca>
2018-07-25 20:44:13 -0400

    Try again with git submodule

--- buildpoco.sh
+++ buildpoco.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
-CDW=$(pwd)
-pushd $(CWD)
-if [ -d poco ]
-then
-    git submodule https://github.com/pocoproject/poco.git poco
-fi
+pushd $(pwd)
+git submodule init
+git submodule update
 cd poco
 # Turn off Apache connector
 bash build_cmake.sh -DPOCO_ENABLE_APACHECONNECTOR=OFF -DPOCO_VERBOSE_MESSAGES=ON -DCMAKE_INSTALL_PREFIX=$(pwd)
